648d18f7610d710480214019fc8f6495c015d751
Túlio Marcos <80016867+Tulio-Marcos@users.noreply.github.com>
2022-08-20 11:31:26 -0300

    Add files via upload

---
+++ Modulo 1/Ex03/script.js
@@ -0,0 +1,11 @@
+//Colocamos Cpf como String por contas da pontação e os outro Caracteres
+var cpf = "115.335.555-88"
+
+// Uma Variavel do tipo Int
+var idade = 25
+
+//Uma Variavel do tipo Bollean
+var Casado =  True 
+
+// Uma Variavel do Tipo Float
+var contas = 55.44
