daa6e4a3cd44b1132b9779db963c8ed19367599e
Túlio Marcos <80016867+Tulio-Marcos@users.noreply.github.com>
2022-09-07 16:09:20 -0300

    Add files via upload

---
+++ Modulo03/script.js
@@ -0,0 +1,18 @@
+function frase () {
+  return " Seja Bem vindo";
+
+}
+
+function somar (valor1,valor2){
+  return valor1 + valor2;
+}
+
+const imc =(peso,altura) => {
+ return( (peso/(altura*altura)))
+}
+
+
+console.log( frase());
+
+console.log( somar(22,55));
+ console.log( imc(90,17.8));
